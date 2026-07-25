3e9ad96160986401e11c5497271cee952cbc3f48
Ahsanullah Qureshi <ahsanullahqureshi888@gmail.com>
2026-07-25 04:30:52 +0430

    fix cors for skybanks

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,9 @@ app.add_middleware(
         "http://127.0.0.1:5173", 
         "http://localhost:5173",
         "https://frontend-sable-ten-54.vercel.app",
-        "https://frontend-qz56kqsg7-sky-ariana-balam-bar-baran.vercel.app"
+        "https://frontend-qz56kqsg7-sky-ariana-balam-bar-baran.vercel.app",
+        "https://sky-banking-frontend.vercel.app",
+        "https://skybanks.vercel.app"
     ],
     allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
